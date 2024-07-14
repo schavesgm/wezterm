c6c1d5a8f9ae86be10c2b9b5fe4e63af1c3ab47c
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2024-07-14 13:53:32 +0100

    feat(base): add wayland support

--- config/base.lua
+++ config/base.lua
@@ -2,6 +2,8 @@ local M = {
     hide_mouse_cursor_when_typing = false,
     audible_bell = "Disabled",
     automatically_reload_config = true,
+    enable_wayland = true,
+    window_decorations = "RESIZE",
 }
 
 return M
