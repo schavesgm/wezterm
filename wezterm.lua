6c3d1e7a54f5774825f10c0b7ad4f6e44ef3f6b5
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2024-07-14 13:54:11 +0100

    feat(wezterm): simplify main script

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,12 @@
-local wezterm_config = {}
+local wezterm = require("wezterm")
+
+---Set the configuration table
+local wezterm_config = {
+    leader = {key = "s", mods = "CTRL", timeout_milliseconds=1000},
+}
 
 ---Add some entries to the configuration
----This function has side-effects on wezterm_config 
+---This function has side-effects on wezterm_config
 ---@param table table; table containing the key values to add.
 local function add_entries_to_config(table)
     for key, value in pairs(table) do
@@ -13,47 +18,16 @@ end
 add_entries_to_config(require("config.base"))
 add_entries_to_config(require("config.fonts"))
 add_entries_to_config(require("config.colours"))
-add_entries_to_config(require("config.keybinds"))
+add_entries_to_config(require("config.keys"))
+
+-- Make sure the window is maximised on start
+wezterm.on("gui-startup", function()
+    local _, _, window = wezterm.mux.spawn_window({})
+    window:gui_window():maximize()
+end)
+
+wezterm_config.unix_domains = {
+    { name = "unix" },
+}
 
 return wezterm_config
-
-
--- return {
---     font = default_font,
---     font_size = 18.0,
---     audible_bell = "Disabled",
---     color_scheme = "kanagawabones",
---     use_fancy_tab_bar = true,
---     window_frame = {
---         font = default_font,
---         font_size = 15.0,
---         active_titlebar_bg = "#1F1F28",
---         inactive_titlebar_bg = "#1F1F28",
---     },
---     colors = {
---         tab_bar = {
---             inactive_tab_edge = "#FF9E3B",
---             active_tab = {
---                 bg_color = "#363646",
---                 fg_color = "#DCD7BA",
---                 intensity = "Half",
---             },
---             inactive_tab = {
---                 bg_color = "#1F1F28",
---                 fg_color = "#C8C093",
---             },
---             inactive_tab_hover = {
---                 bg_color = "#1F1F28",
---                 fg_color = "#FF9E3B",
---             },
---             new_tab = {
---                 bg_color = "#1F1F28",
---                 fg_color = "#76946A",
---             },
---             new_tab_hover = {
---                 bg_color = "#1F1F28",
---                 fg_color = "#98BB6C",
---             }
---         }
---     },
--- }
